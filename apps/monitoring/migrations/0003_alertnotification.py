cbe846fe88fb00e2a3df7f76193f58865efe6b28
Trayan Lichev <teron@examle.com>
2026-06-16 18:08:59 +0300

    migrations fix

--- apps/monitoring/migrations/0003_alertnotification.py
+++ apps/monitoring/migrations/0003_alertnotification.py
@@ -5,7 +5,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('monitoring', '0002_rename_monitoring_a_status_severity_idx_monitoring__status_d7b628_idx_and_more'),
+        ('monitoring', '0001_initial'),
     ]
 
     operations = [
